2bb9b082bb63a7f32bc7bf233e39a2ad127419b0
haryanto.duwi <haryanto.duwi@gmail.com>
2020-06-07 10:54:09 +0700

    add--tab dashboard

--- application/modules/Dashboard/views/Konten.php
+++ application/modules/Dashboard/views/Konten.php
@@ -1,94 +1,121 @@
 <div class="row">
-  <span id="urldashboard" url="<?=$url.'/getdata'?>"></span>
-  <div class="col-sm-6">
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-warning">
-            <i class="far fa-file"></i>
+  <div class="col-sm-12">
+    <div class="card">
+      <div class="card-body">
+        <ul class="nav nav-tabs" id="myTab2" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="home-tab2" data-toggle="tab" href="#home2" role="tab" aria-controls="home" aria-selected="true">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="profile-tab2" data-toggle="tab" href="#profile2" role="tab" aria-controls="profile" aria-selected="false">Sistem</a>
+          </li>
+        </ul>
+        <div class="tab-content tab-bordered" id="myTab3Content">
+          <div class="tab-pane fade show active" id="home2" role="tabpanel" aria-labelledby="home-tab2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Jumlah Pengguna</h4>
-            </div>
-            <div class="card-body">
-              <span id="jumlahuser"></span>
+          <div class="tab-pane fade" id="profile2" role="tabpanel" aria-labelledby="profile-tab2">
+            <div class="row">
+              <span id="urldashboard" url="<?=$url.'/getdata'?>"></span>
+              <div class="col-sm-6">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="card card-statistic-1">
+                      <div class="card-icon bg-warning">
+                        <i class="far fa-file"></i>
+                      </div>
+                      <div class="card-wrap">
+                        <div class="card-header">
+                          <h4>Jumlah Pengguna</h4>
+                        </div>
+                        <div class="card-body">
+                          <span id="jumlahuser"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="card card-statistic-1">
+                      <div class="card-icon bg-warning">
+                        <i class="far fa-file"></i>
+                      </div>
+                      <div class="card-wrap">
+                        <div class="card-header">
+                          <h4>Log</h4>
+                        </div>
+                        <div class="card-body">
+                          <span id="log"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="card">
+                      <div class="card-header">
+                        <h4>Akses Sistem</h4>
+                      </div>
+                      <div class="card-body">
+                        <canvas id="myChart2"></canvas>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="card card-statistic-1">
+                      <div class="card-icon bg-warning">
+                        <i class="fas fa-server"></i>
+                      </div>
+                      <div class="card-wrap">
+                        <div class="card-header">
+                          <h4>Upload</h4>
+                        </div>
+                        <div class="card-body">
+                          <span><?=$folderupload.' Mb'?></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="card card-statistic-1">
+                      <div class="card-icon bg-warning">
+                        <i class="fas fa-upload"></i>
+                      </div>
+                      <div class="card-wrap">
+                        <div class="card-header">
+                          <h4>Backup</h4>
+                        </div>
+                        <div class="card-body">
+                          <span><?=$folderberkas.' Mb'?></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12">
+                    <div class="card">
+                      <div class="card-header">
+                        <h4>User</h4>
+                      </div>
+                      <div class="card-body">
+                        <canvas id="chartuser"></canvas>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm-6">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-warning">
-            <i class="far fa-file"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Log</h4>
-            </div>
-            <div class="card-body">
-              <span id="log"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="card">
-          <div class="card-header">
-            <h4>Akses Sistem</h4>
-          </div>
-          <div class="card-body">
-            <canvas id="myChart2"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-warning">
-            <i class="fas fa-server"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Upload</h4>
-            </div>
-            <div class="card-body">
-              <span><?=$folderupload.' Mb'?></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-warning">
-            <i class="fas fa-upload"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Backup</h4>
-            </div>
-            <div class="card-body">
-              <span><?=$folderberkas.' Mb'?></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <div class="card">
-          <div class="card-header">
-            <h4>User</h4>
-          </div>
-          <div class="card-body">
-            <canvas id="chartuser"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 <script type="text/javascript">
